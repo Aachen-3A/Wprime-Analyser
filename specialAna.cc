1b0e439021301babff9761eda4f155d34390cf93
michael magros <michael.magros@rwth-aachen.de>
2014-10-21 14:54:34 +0200

    abs(PdgNumber)

--- specialAna.cc
+++ specialAna.cc
@@ -935,13 +935,13 @@ void specialAna::Fill_Particle_hisos(int hist_number, pxl::Particle* lepton , st
 void specialAna::Fill_Controll_histo(int hist_number, pxl::Particle* lepton) {
 
     //if(lepton->getUserRecord("id").toInt32()==11){
-    if(lepton->getPdgNumber()==11){
+    if(TMath::Abs(lepton->getPdgNumber())==11){
         Fill_Controll_Ele_histo(hist_number, lepton);
     }
-    if(lepton->getPdgNumber()==13){
+    if(TMath::Abs(lepton->getPdgNumber())==13){
         Fill_Controll_Muon_histo(hist_number, lepton);
     }
-    if(lepton->getPdgNumber()==15){
+    if(TMath::Abs(lepton->getPdgNumber())==15){
         Fill_Controll_Tau_histo(hist_number, lepton);
     }
 }
@@ -1188,7 +1188,7 @@ void specialAna::endEvent( const pxl::Event* event ){
    }
 }
 
-void specialAna::SetEvents(int e){
-    events_=e;
-}
+//void specialAna::SetEvents(int e){
+    //events_=e;
+//}
 
